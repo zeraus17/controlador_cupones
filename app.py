b8ba596087f88105274183007a4d4a4ee73b60e2
Agus <agustin_suarez1324@outlook.com>
2025-09-15 22:26:39 -0300

    Agrego dependencias para Flask

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template("index.html")
-
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/")
+def home():
+    return "Hola desde Flask en Vercel"
